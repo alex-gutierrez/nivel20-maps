25fba7c1688cb3b0c57b65976de725f25b20a472
Álex Gutiérrez <alejandro.gutierrez@twincoders.com>
2025-11-12 19:43:08 +0100

    Initial commit

---
+++ tilesets/lpc-collection/fence_medieval.tsx
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="fence_medieval" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <image source="fence_medieval.png" width="512" height="1024"/>
+ <wangsets>
+  <wangset name="Fence 1_Beam" type="edge" tile="-1">
+   <wangcolor name="beam" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+  </wangset>
+  <wangset name="Fence 2_Round" type="edge" tile="-1">
+   <wangcolor name="round" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="3" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="4" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="5" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="19" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="20" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="21" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="35" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="36" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="37" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="51" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="52" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="53" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="67" wangid="1,0,0,0,2,0,2,0"/>
+   <wangtile tileid="68" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="69" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 3_Plank" type="edge" tile="-1">
+   <wangcolor name="plank" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="7" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="8" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="22" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="23" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="24" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="38" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="39" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="40" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="54" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="55" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="56" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="70" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="71" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="72" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 4_Picket" type="edge" tile="-1">
+   <wangcolor name="picket fence" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="9" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="10" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="11" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="25" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="26" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="27" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="41" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="42" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="43" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="57" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="58" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="59" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="73" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="74" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="75" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 5_Rope" type="edge" tile="-1">
+   <wangcolor name="rope" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="12" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="13" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="14" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="28" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="29" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="30" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="44" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="45" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="46" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="60" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="61" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="62" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="76" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="77" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="78" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 6_Picket_Dense" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="96" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="97" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="98" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="112" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="113" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="114" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="128" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="129" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="130" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="144" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="145" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="146" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="160" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="161" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="162" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 7_Crooked" type="edge" tile="-1">
+   <wangcolor name="crooked fence" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="99" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="100" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="101" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="115" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="116" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="117" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="131" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="132" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="133" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="147" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="148" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="149" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="163" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="164" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="165" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 8_Rock" type="edge" tile="-1">
+   <wangcolor name="rock" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="102" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="103" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="104" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="118" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="119" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="120" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="134" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="135" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="136" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="150" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="151" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="152" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="166" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="167" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="168" wangid="1,0,2,0,2,0,1,0"/>
+  </wangset>
+  <wangset name="Fence 9_Palisades" type="edge" tile="-1">
+   <wangcolor name="palisade" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="105" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="106" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="121" wangid="1,0,2,0,2,0,2,0"/>
+   <wangtile tileid="122" wangid="1,0,2,0,1,0,2,0"/>
+   <wangtile tileid="123" wangid="2,0,2,0,1,0,2,0"/>
+   <wangtile tileid="137" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="138" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="139" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="153" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="154" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="155" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="169" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="170" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="171" wangid="1,0,2,0,2,0,1,0"/>
+   <wangtile tileid="185" wangid="2,0,2,0,2,0,1,0"/>
+   <wangtile tileid="187" wangid="2,0,1,0,2,0,2,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
